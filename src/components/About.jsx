d8a3bfb8341aed1f0ad2ce9b6fea4c8505e970e3
Akshit Parmar <akshitparmar25990@gmail.com>
2022-09-11 03:13:24 +0530

    Add existing project files to Git

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+  <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+   <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+     <div className='pb-8'>
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+     </div>
+        <p className='text-xl mt-20 '>
+
+        My name is Akshit Parmar .I did my high school and intermediate from  CBSE board with 93 and 88% respectively.
+        I am pursuing B-Tech in computer science and engineering from Srm institute of science and techonology.
+        My hobbies are playing some outdoor games like cricket for physical fitness, listening to music and watching crime and investigation series.
+        My strength is hard-working, self-confidence, adaptable in any kind of situation and team work.
+        My weakness is I am uncomfortable until I finished my work in the given time i.e i can not enjoy moments till my work is not completed .
+        My short-term goal is to get a job in a reputed company.
+        right now i work as a web developer as i have done 2 internship in this field .
+        I know how to work in a team or how to manange time and i know how to work together to achieve a common goal .
+        My long-term goal is to achieve a good position where I build my career and organizations too.
+        I am also the core member of the ISTE (indian society of techincal eduction )of my college
+        That's all about me.
+
+        </p>
+        <br/>
+
+        <p className='text-xl'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium neque nisi accusamus porro laborum quis, enim numquam at incidunt quidem unde vitae. Dolore eius fugit explicabo dolor minus eum obcaecati sapiente, possimus molestiae, voluptatem est odit magni. Quisquam porro voluptatibus consequuntur fugiat, accusamus veritatis excepturi molestiae quos? Officia, perferendis explicabo.
+        </p>
+    </div>
+
+ 
+
+  </div>
+    
+ 
+  )
+};
+
+export default About;
