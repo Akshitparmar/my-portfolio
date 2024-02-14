38b47149217e182477ae3086574e20ed606d6ae5
Akshit Parmar <72138022+Akshitparmar@users.noreply.github.com>
2024-02-14 15:59:44 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a front-end Developer</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Final Year B.Tech student</h2>
             <p className='text-gray-500 py-4 max-w-md'>
                 I have  6 months of Experience building and desigining 
                 software.
