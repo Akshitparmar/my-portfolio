ebb976d465076a88bcd3dd0a2b8ff3b6fbf8d848
Akshit Parmar <72138022+Akshitparmar@users.noreply.github.com>
2024-02-14 16:15:00 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,8 +3,6 @@ import html from '../assets/html.png';
 import css from '../assets/css.png';
 import javascript from '../assets/javascript.png';
 import reactImage from '../assets/react.png';
-import nextjs from '../assets/nextjs.png';
-import graphql from '../assets/graphql.png';
 import github from '../assets/github.png';
 import tailwind from '../assets/tailwind.png';
 import cpp from '../assets/c++.png';
